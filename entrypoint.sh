985aceca5237952863003cd6d5e3d2415eb18d96
Chris Plaatjes <kizaing@gmail.com>
2021-07-20 17:24:46 -0400

    Fixed sed line terminator error for database name

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ fi
 
 if grep -q 'db_name_here' /KavitaStats/appsettings.json
 then
-    sed -i "s/db_name_here/${DB_NAME}" /KavitaStats/appsettings.json
+    sed -i "s/db_name_here/${DB_NAME}/g" /KavitaStats/appsettings.json
 fi
 
 if grep -q 'api_key_here' /KavitaStats/appsettings.json
