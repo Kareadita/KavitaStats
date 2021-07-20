7a46d6edd40fcd1ef2a0ab1650193c5862d1e2fc
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2021-07-20 15:29:49 -0500

    Added some comments based on observations from data.

--- Application/Domain/InstallationStatistics/ServerInfo.cs
+++ Application/Domain/InstallationStatistics/ServerInfo.cs
@@ -4,9 +4,17 @@
     {
         public string Os { get; set; }
         public string DotNetVersion { get; set; }
+        /// <summary>
+        /// NOTE: This is the same thing as DotNetVersion
+        /// </summary>
         public string RunTimeVersion { get; set; }
         public string KavitaVersion { get; set; }
         public string BuildBranch { get; set; }
+        /// <summary>
+        /// Culture tag of the machine.
+        /// <remarks>Culture will be empty if used within a Docker image. This is a limitation of alpine linux.
+        /// https://github.com/dotnet/dotnet-docker/issues/533</remarks>
+        /// </summary>
         public string Culture { get; set; }
         public bool IsDocker { get; set; }
         public int NumOfCores { get; set; }
