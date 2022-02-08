e4803f6ce36fe2ee43a4100136e5143e1801ffb4
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2022-02-08 14:55:21 -0600

    Removed some demo code for stats grafana and generated the migration

--- KavitaStats/Data/Migrations/20220208205249_BookmarksLibNumber.Designer.cs
+++ KavitaStats/Data/Migrations/20220208205249_BookmarksLibNumber.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using KavitaStats.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KavitaStats.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220208205249_BookmarksLibNumber")]
+    partial class BookmarksLibNumber
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.0");
